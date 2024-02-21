3fbaac61355fd274e38c991462d1698283687e50
DivaAji <diva7aji7@gmail.com>
2024-02-21 08:56:51 +0700

    Js1 P1,2,3

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,61 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+    });
+
+Route::get('/posts/{post}/comments/{comment}', function
+    ($postId, $commentId) {
+     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+    });
+
+Route::get('/articles/{id}', function ($id) {
+    return "Halaman Artikel dengan ID " . $id;
+});
+
+// Route::get('/user/{name?}', function ($name=null) {
+//     return 'Nama saya '.$name;
+// });
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+    });
+
+Route::get('/user/profile', function() {
+       //
+})->name('profile');
+
+//Route Group    
+Route::middleware(['first', 'second'])->group(function () {
+    Route::get('/', function () {
+    // Uses first & second middleware...
+});
+Route::get('/user/profile', function () {
+ // Uses first & second middleware...
+ });
+});
+Route::domain('{account}.example.com')->group(function () {
+ Route::get('user/{id}', function ($account, $id) {
+ //
+ });
+});
+Route::middleware('auth')->group(function () {
+Route::get('/user', [UserController::class, 'index']);
+Route::get('/post', [PostController::class, 'index']);
+Route::get('/event', [EventController::class, 'index']);
+});
+
+//Route Prefixes
+Route::prefix('admin')->group(function () {
+    Route::get('/user', [UserController::class, 'index']);
+    Route::get('/post', [PostController::class, 'index']);
+    Route::get('/event', [EventController::class, 'index']);
+    });
+
+//Redirect Routes
+Route::redirect('/here', '/there');
+
+//View Routes
+Route::view('/welcome', 'welcome');
+Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
